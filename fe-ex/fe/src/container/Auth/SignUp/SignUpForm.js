d9531958d36b035f70ccc12073feedd7b9cf02e7
Developer Park's Codes <pakjkwan@gmail.com>
2021-03-05 14:02:20 +0900

    14

--- fe-ex/fe/src/container/Auth/SignUp/SignUpForm.js
+++ fe-ex/fe/src/container/Auth/SignUp/SignUpForm.js
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import { useForm, Controller } from 'react-hook-form';
 import { MdLockOpen } from 'react-icons/md';
@@ -9,42 +10,53 @@ import { FieldWrapper, SwitchWrapper, Label } from '../Auth.style';
 import {TextField} from '@material-ui/core'
 const SignUpForm = () => {
 
-  const {userid, setUserid} = useState('')
-  const {password, setPassword} = useState('')
-  const {username, setUsername} = useState('')
-  const {email, setEmail} = useState('')
-  const {gender, setGender} = useState('')
-  const {birthday, setBirthday} = useState('')
-  const {phoneNumber, setPhoneNumber} = useState('')
-  const {preferGenre, setPreferGenre} = useState('')
-
- /* Original Form */ 
-  const { signUp, loggedIn } = useContext(AuthContext);
-
- 
-  const { control, watch, errors, handleSubmit, confirmPassword }  = {}
-
+  const [userid, setUserid] = useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [gender, setGender] = useState('')
+  const [birthday, setBirthday] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [preferGenre, setPreferGenre] = useState('')
+  const register = e => {
+    e.preventDefault()
+    axios.post(`/users/save`,{
+      userid, password, username, email, gender, birthday, phoneNumber,preferGenre
+    })
+    .then(resp => {})
+    .catch(err => {})
+  }
   return (
     <>
       <div>
           <label>User ID</label>
-          <TextField name="userid" required /><br/>
+          <TextField name="userid" required 
+            onChange = { e => {setUserid(`${e.target.value}`) }}
+            /><br/>
           <label>Name</label>
-          <TextField name="username" required />
+          <TextField name="username" 
+          onChange = { e => { setUsername(`${e.target.value}`) }}
+          required />
         </div>
         <div>
           <label>Email Address</label>
-          <TextField name="email" required />
+          <TextField name="email" required 
+          onChange = { e => { setEmail(`${e.target.value}`) }}
+          />
         </div>
         <div>
           <label>Password</label>
-          <TextField name="password"/>
+          <TextField name="password"
+          onChange = { e => { setPassword(`${e.target.value}`) }}
+          />
         </div>
         <div>
           <label>Birthday</label>
-          <TextField name="birthday"/>
+          <TextField name="birthday"
+          onChange = { e => { setBirthday(`${e.target.value}`) }}
+          />
         </div>
-        <button type="submit">Sign Up</button>
+        <button type="submit" onClick= {register}>Sign Up</button>
       </>
   );
 };
