c9813e2cc9f7095d54ff2c1d47aa077bc1b894a6
Developer Park's Codes <pakjkwan@gmail.com>
2021-03-08 17:09:23 +0900

    17

--- be/demo/src/main/java/com/example/demo/uss/service/UserService.java
+++ be/demo/src/main/java/com/example/demo/uss/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.uss.service;
 
-public interface UserService {
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService{
 
 }
